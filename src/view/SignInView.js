88bede59312dd6ad9632425706ae3c66d6291b2c
arasto <Arasto.Sahbaei@gmail.com>
2020-10-21 13:36:14 +0200

    login

--- src/view/SignInView.js
+++ src/view/SignInView.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 
 export const SignInView = () => {
 
-    const [loggedInUser, setLoggedInUser] = useState("Arasto")
+    const [loggedInUser, setLoggedInUser] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <div>
-            <h1>{loggedInUser}</h1>
-            <button onClick={() => setLoggedInUser("Hulken")}>Update the state value!</button>
+            <span>Username: </span><input onChange={event => setLoggedInUser(event.target.value)} /> <br />
+            <span>Password: </span><input type="password" onChange={event => setPassword(event.target.value)} /> <br />
+            <button>Login</button>
         </div >
     )
 }
