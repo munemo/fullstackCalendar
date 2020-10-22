2d01863c50216e1326872f3853adab3770815c47
arasto <Arasto.Sahbaei@gmail.com>
2020-10-22 08:46:45 +0200

    m

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { RecipeView } from '../view/RecipeView'
 import { HomeView } from '../view/HomeView'
 import { SignInView } from '../view/SignInView'
+import { UserContext } from '../shared/global/provider/UserProvider'
+
 
 export const Routing = (props) => {
+    const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
+
+    const checkIfUserIsAuthenticatedInBrowser = () => {
+        setAuthenticatedUser(localStorage.getItem("username"))
+    }
+
+    useEffect(() => {
+        checkIfUserIsAuthenticatedInBrowser()
+    })
+
     return (
         <Router>
             {props.children}
