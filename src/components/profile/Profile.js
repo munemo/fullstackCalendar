a00747286f9fe3b084ac46c54ff9aa5f8c808ada
arasto <Arasto.Sahbaei@gmail.com>
2020-10-22 13:16:26 +0200

    m

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,14 +1,28 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../../shared/global/provider/UserProvider'
+import { useHistory } from 'react-router-dom'
 import './Profile.css'
 
 export const Profile = () => {
+    const history = useHistory()
     const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
+
+    const logout = () => {
+        localStorage.removeItem('username')
+        setAuthenticatedUser(false)
+        history.push('/')
+    }
 
     return (
         <div className="profileWrapper">
             <img className="profileImg" src={"https://www.thispersondoesnotexist.com/image"} alt="" />
             <span className="displayedUsername">{authenticatedUser}</span>
+            <div className="profileDropdown">
+                <a onClick={() => history.push('/settings')}>Settings</a>
+                <a onClick={() => history.push('/profile')}>Profile</a>
+                <hr />
+                <a onClick={() => logout()}>Logout</a>
+            </div>
         </div>
     )
 }
--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -4,6 +4,8 @@ import { RecipeView } from '../view/RecipeView'
 import { HomeView } from '../view/HomeView'
 import { SignInView } from '../view/SignInView'
 import { UserContext } from '../shared/global/provider/UserProvider'
+import { ProfileView } from '../view/ProfileView'
+import { SettingsView } from '../view/SettingsView'
 
 
 export const Routing = (props) => {
@@ -23,6 +25,8 @@ export const Routing = (props) => {
             <Switch>
                 <Route exact path="/recipe" component={RecipeView} />
                 <Route exact path="/signin" component={SignInView} />
+                <Route exact path="/profile" component={ProfileView} />
+                <Route exact path="/settings" component={SettingsView} />
                 <Route component={HomeView} />
             </Switch>
         </Router>
