90d00ecb5d04ebdbaa0037e5e8b090cef953caf1
arasto <Arasto.Sahbaei@gmail.com>
2020-10-30 09:09:30 +0100

    m

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
+const port = process.env.PORT
 
-app.listen(3001, () => {
-    console.log("Servern är igång på port 3001")
+app.listen(port, () => {
+    console.log(`Servern är igång på port ${port}`)
 })
