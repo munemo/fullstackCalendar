d1a64425181a6fc8cd91227d8b94b8ef720a3400
arasto <Arasto.Sahbaei@gmail.com>
2020-10-30 14:50:41 +0100

    m

--- server/Server.js
+++ server/Server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import helmet from 'helmet'
 
 dotenv.config()
 const app = express()
+app.use(helmet())
 const port = process.env.PORT
 
 app.get('/recipe', (req, res) => {
