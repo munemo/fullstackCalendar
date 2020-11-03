ffa14d0295015ab7a1a85fbf7d752f7441969810
arasto <Arasto.Sahbaei@gmail.com>
2020-11-03 12:13:20 +0100

    m

--- client/src/shared/api/PokemonAPI.js
+++ client/src/shared/api/PokemonAPI.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const PokemonAPI = Axios.create({
-    baseURL: 'https://pokeapi.co/api/v2/pokemon'
+    baseURL: 'https://pokeapi.co/api/v2/pokemon',
 })
 
 export default PokemonAPI
--- server/Server.js
+++ server/Server.js
@@ -7,11 +7,22 @@ dotenv.config()
 const app = express()
 app.use(helmet())
 app.use(morgan('common'))
+/* app.use(isAuthenticated) */
 const port = process.env.PORT
 
 app.get('/recipe', (req, res) => {
 	res.send('Pancakes!')
 })
+
+app.get('/user', isAuthenticated, (req, res) => {
+})
+
+function isAuthenticated(req, res, next) {
+	req.query.admin === 'true'
+		? res.send('You are admin')
+		: res.send('You cannot make calls to this API URL')
+	next()
+}
 
 app.listen(port, () => {
 	console.log(`Servern är igång på port ${port}`)
