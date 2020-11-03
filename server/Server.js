bee24221b8d9c4bb45f9836840f9c862d611b201
arasto <Arasto.Sahbaei@gmail.com>
2020-11-03 13:08:30 +0100

    m

--- server/Server.js
+++ server/Server.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(helmet())
 app.use(morgan('common'))
 /* app.use(isAuthenticated) */
+
 const port = process.env.PORT
 
 app.get('/recipe', (req, res) => {
@@ -17,11 +18,19 @@ app.get('/recipe', (req, res) => {
 app.get('/user', isAuthenticated, (req, res) => {
 })
 
+app.use(notFound)
+
 function isAuthenticated(req, res, next) {
 	req.query.admin === 'true'
 		? res.send('You are admin')
 		: res.send('You cannot make calls to this API URL')
 	next()
+}
+
+function notFound(req, res, next) {
+	const error = new Error(`Not found: ${req.originalUrl}`)
+	res.status(404)
+	next(error)
 }
 
 app.listen(port, () => {
