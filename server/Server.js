ed6b59f1f858453fe810992ac3bfe0c468a4826d
arasto <Arasto.Sahbaei@gmail.com>
2020-11-04 17:11:17 +0100

    m

--- server/Server.js
+++ server/Server.js
@@ -2,47 +2,21 @@ import express from 'express'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import middlewares from './src/middlewares/Middlewares.js'
 
 dotenv.config()
 const app = express()
+const port = process.env.PORT
+
 app.use(helmet())
 app.use(morgan('common'))
-/* app.use(isAuthenticated) */
-
-const port = process.env.PORT
 
 app.get('/recipe', (req, res) => {
 	res.send('Pancakes!')
 })
 
-app.get('/user', isAuthenticated, (req, res) => {
-})
-
-app.use(notFound)
-app.use(errorHandler)
-
-function isAuthenticated(req, res, next) {
-	req.query.admin === 'true'
-		? res.send('You are admin')
-		: res.send('You cannot make calls to this API URL')
-	next()
-}
-
-function notFound(req, res, next) {
-	const error = new Error(`Not found: ${req.originalUrl}`)
-	res.status(404)
-	next(error)
-}
-
-function errorHandler(error, req, res, next) {
-	const statuscode = res.statusCode === 200 ? 500 : res.statusCode
-	res.status(statuscode)
-	res.json({
-		statuscode: statuscode,
-		message: error.message,
-		stacktrace: error.stack,
-	})
-}
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler)
 
 app.listen(port, () => {
 	console.log(`Servern är igång på port ${port}`)
---
+++ server/src/middlewares/Middlewares.js
@@ -0,0 +1,20 @@
+const notFound = (req, res, next) => {
+	const error = new Error(`Not found: ${req.originalUrl}`)
+	res.status(404)
+	next(error)
+}
+
+const errorHandler = (error, req, res, next) => {
+	const statuscode = res.statusCode === 200 ? 500 : res.statusCode
+	res.status(statuscode)
+	res.json({
+		statuscode: statuscode,
+		message: error.message,
+		stacktrace: error.stack,
+	})
+}
+
+export default {
+	notFound,
+	errorHandler
+}
