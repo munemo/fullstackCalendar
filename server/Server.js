3b7410aaabafa4ab9cc13bb1f37698588cf94813
arasto <Arasto.Sahbaei@gmail.com>
2020-11-05 14:24:28 +0100

    m

--- server/Server.js
+++ server/Server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import mongoose from 'mongoose'
 import middlewares from './src/middlewares/Middlewares.js'
 
 dotenv.config()
@@ -17,6 +18,13 @@ app.get('/recipe', (req, res) => {
 
 app.use(middlewares.notFound)
 app.use(middlewares.errorHandler)
+
+mongoose.connect('mongodb://localhost/arastotutorialdatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('Successfully connected to the database!'))
+	.catch((error) => {
+		console.log('ERROR WHILE TRYING TO CONNECT TO THE DATABASE: ', error)
+		process.exit()
+	})
 
 app.listen(port, () => {
 	console.log(`Servern är igång på port ${port}`)
