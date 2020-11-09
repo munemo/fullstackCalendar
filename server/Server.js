c51889625dd33c6819e1f6190c6d4f996259bd3e
arasto <Arasto.Sahbaei@gmail.com>
2020-11-09 13:18:43 +0100

    controller

--- server/Server.js
+++ server/Server.js
@@ -2,12 +2,14 @@ import express from 'express'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import bodyParser from 'body-parser'
 import middlewares from './src/middlewares/Middlewares.js'
 import Configuration from './configuration/Configuration.js'
 
 dotenv.config()
 const app = express()
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(helmet())
 app.use(morgan('common'))
 
---
+++ server/src/controllers/User.controller.js
@@ -0,0 +1,21 @@
+import UserModel from '../models/User.model.js'
+
+const createUser = async (req, res) => {
+
+	const user = new UserModel({
+		username: req.body.username,
+		password: req.body.password
+	})
+
+	try {
+		const response = user.save()
+		res.status(201).send(response)
+	} catch (error) {
+		res.status(500).send({ message: error.message })
+	}
+
+}
+
+export default {
+	createUser
+}
