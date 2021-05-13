f1713fcaeb7db39c41c886c6d87a69e2725a0c90
munemo <john.chimbani@outlook.com>
2021-05-13 09:04:54 +0200

    Initial project setup for server instance with database

--- server/Server.js
+++ server/Server.js
@@ -1,18 +1,17 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
-import bodyParser from 'body-parser'
+import cors from 'cors'
 import middlewares from './src/middlewares/Middlewares.js'
 import Configuration from './configuration/Configuration.js'
 import UserRoutes from './src/routes/User.routes.js'
 
-dotenv.config()
 const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 app.use(helmet())
+app.use(express.json())
+app.use(cors({credential: true}))
 app.use(morgan('common'))
+app.use(helmet())
 
 app.get('/recipe', (req, res) => {
 	res.send('Pancakes!')
--- server/configuration/Configuration.js
+++ server/configuration/Configuration.js
@@ -2,23 +2,35 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
+const { PORT, DEV_DATABASE_URL,
+    PROD_DATABASE_URL,
+    ENVIRONMENT } = process.env
 
-const connectToDatabase = async () => {
+const connectToPort = async (app) => {
 	try {
-		const DB_URL = process.env.DATABASE_URL
-		await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-		console.log('SUCESSFULLY CONNECTED TO THE DATABASE!')
+		await app.listen(PORT, () => {
+			console.log(`✔️ SERVER RUNNING ON ${PORT}`);
+		})
 	} catch (error) {
-		console.log('ERROR WHILE TRYING TO CONNECT TO THE DATABASE: ', error)
-		process.exit()
+		console.error(`❌  ERROR OCCURED WHILE CONNECTING TO ${PORT}`)
 	}
 }
 
-const connectToPort = (app) => {
-	const port = process.env.PORT
-	app.listen(port, () => {
-		console.log(`SERVER IS RUNNING ON PORT: ${port}`)
-	})
+const connectToDatabase = async () => {
+	const DATABASE_URL = ENVIRONMENT === 'DEVELOPMENT' ? DEV_DATABASE_URL : PROD_DATABASE_URL
+	try {
+		await mongoose.connect(`${DATABASE_URL}`,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useCreateIndex: true
+			})
+		console.log(`✔️ CONNECTED TO DATABASE`);
+
+	} catch (error) {
+		console.error(`❌  ERROR OCCURED WHILE CONNECTING TO DATABASE`)
+		process.exit()
+	}
 }
 
 export default {
